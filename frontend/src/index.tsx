ed9a923bad6e8590f0bf1af0261e95cc9b69dd82
azamim316-svg <azamim316@gmail.com>
2026-05-16 18:00:41 -0400

    fix index.tsx networks

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,22 +4,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 const classicNetworks = {
-  mainnet: {
-    "columbus-5": {
-      chainID: "columbus-5",
-      lcd: "https://terra-classic-lcd.publicnode.com",
-      gasAdjustment: 1.4,
-      gasPrices: { uluna: 28.325 },
-      prefix: "terra",
-    },
+  "columbus-5": {
+    chainID: "columbus-5",
+    lcd: "https://terra-classic-lcd.publicnode.com",
+    gasAdjustment: 1.4,
+    gasPrices: { uluna: 28.325 },
+    prefix: "terra",
   },
 };
 
-getInitialConfig().then((config) => {
-  ReactDOM.render(
-    <WalletProvider defaultNetworks={classicNetworks}>
-      <App />
-    </WalletProvider>,
-    document.getElementById("root")
-  );
-});
+ReactDOM.render(
+  <WalletProvider defaultNetworks={classicNetworks as unknown as any}>
+    <App />
+  </WalletProvider>,
+  document.getElementById("root")
+);
